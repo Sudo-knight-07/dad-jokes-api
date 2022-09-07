d91ada378e6a51433086677a4d015310f63d6cf0
Vaibhav <nightwalker.alon@gmail.com>
2022-09-07 22:30:51 +0530

    fetch api

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const nextBtn = document.getElementById('nextBtn');
+const jokes = document.getElementById('joke')
 
+//generating random joke
+// api --->  GET https://icanhazdadjoke.com/
+
+const generateJoke = () => {
+  const setHeader = {
+    headers: {
+      Accept: "application/json"
+    }
+  }
+  fetch('https://icanhazdadjoke.com/', setHeader)
+    .then((res) => res.json())
+    .then((response) => {
+      jokes.innerHTML = response.joke;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+}
+
+nextBtn.addEventListener('click', generateJoke);
+generateJoke();
