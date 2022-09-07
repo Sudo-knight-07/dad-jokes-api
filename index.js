ee772967d4d71c286187c595fd370bc3c47beeac
Vaibhav <nightwalker.alon@gmail.com>
2022-09-07 22:35:08 +0530

    async and await

--- index.js
+++ index.js
@@ -4,20 +4,38 @@ const jokes = document.getElementById('joke')
 //generating random joke
 // api --->  GET https://icanhazdadjoke.com/
 
-const generateJoke = () => {
-  const setHeader = {
-    headers: {
-      Accept: "application/json"
+// const generateJoke = () => {
+//   const setHeader = {
+//     headers: {
+//       Accept: "application/json"
+//     }
+//   }
+//   fetch('https://icanhazdadjoke.com/', setHeader)
+//     .then((res) => res.json())
+//     .then((response) => {
+//       jokes.innerHTML = response.joke;
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+// }
+
+//using async and await
+
+const generateJoke = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json"
+      }
     }
+    const res = await fetch('https://icanhazdadjoke.com/', setHeader);
+    const data = await res.json();
+    jokes.innerHTML = data.joke;
+  } catch {
+    (error)
+    console.log(error);
   }
-  fetch('https://icanhazdadjoke.com/', setHeader)
-    .then((res) => res.json())
-    .then((response) => {
-      jokes.innerHTML = response.joke;
-    })
-    .catch((error) => {
-      console.log(error);
-    })
 }
 
 nextBtn.addEventListener('click', generateJoke);
